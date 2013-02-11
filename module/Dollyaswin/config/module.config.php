7a9f26182620e648cfb026fd43a7b1802fe2861f
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2013-02-11 17:37:28 +0700

    No child route for login

--- module/Dollyaswin/config/module.config.php
+++ module/Dollyaswin/config/module.config.php
@@ -40,21 +40,6 @@ return array(
                         'action'        => 'login',
                     ),
                 ),
-                'may_terminate' => true,
-                'child_routes' => array(
-                    'default' => array(
-                        'type'    => 'Segment',
-                        'options' => array(
-                            'route'    => '/[:controller[/:action]]',
-                            'constraints' => array(
-                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            ),
-                            'defaults' => array(
-                            ),
-                        ),
-                    ),
-                ),
             ),
         ),
     ),
